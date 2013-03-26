e1c6616732dc160d2e99b33e3715b461d760a810
mht <qrnnis2623891@gmail.com>
2013-03-26 18:22:51 +0800

    update

--- application/views/login_view.php
+++ application/views/login_view.php
@@ -8,26 +8,26 @@
     <meta name="author" content="">
 
     <!-- Le styles -->
-    <link href='<?=base_url().'bootstrap/assets/css/bootstrap.css'?>' type="text/css" rel = "stylesheet"></link>
+    <link href='<?=base_url().'bootstrap/css/bootstrap.css'?>' type="text/css" rel = "stylesheet"></link>
     <style type="text/css">
       body {
         padding-top: 60px;
         padding-bottom: 40px;
       }
     </style>
-    <link href='<?=base_url().'bootstrap/assets/css/bootstrap-reponsive.css'?>' type="text/css" rel = "stylesheet"></link>
+    <link href='<?=base_url().'bootstrap/css/bootstrap-reponsive.css'?>' type="text/css" rel = "stylesheet"></link>
 
     <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
     <!--[if lt IE 9]>
       <script src="assets/js/html5shiv.js"></script>
     <![endif]-->
 
-    <!-- Fav and touch icons -->
-    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="assets/ico/apple-touch-icon-144-precomposed.png">
-    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="assets/ico/apple-touch-icon-114-precomposed.png">
-      <link rel="apple-touch-icon-precomposed" sizes="72x72" href="assets/ico/apple-touch-icon-72-precomposed.png">
-                    <link rel="apple-touch-icon-precomposed" href="assets/ico/apple-touch-icon-57-precomposed.png">
-                                   <link rel="shortcut icon" href="assets/ico/favicon.png">
+    <!--[> Fav and touch icons <]-->
+    <!--<link rel="apple-touch-icon-precomposed" sizes="144x144" href="assets/ico/apple-touch-icon-144-precomposed.png">-->
+    <!--<link rel="apple-touch-icon-precomposed" sizes="114x114" href="assets/ico/apple-touch-icon-114-precomposed.png">-->
+      <!--<link rel="apple-touch-icon-precomposed" sizes="72x72" href="assets/ico/apple-touch-icon-72-precomposed.png">-->
+                    <!--<link rel="apple-touch-icon-precomposed" href="assets/ico/apple-touch-icon-57-precomposed.png">-->
+                                   <!--<link rel="shortcut icon" href="assets/ico/favicon.png">-->
   </head>
 
   <body>
@@ -70,33 +70,33 @@
       </div>
     </div>
 
-    <div class="container">
+    <!--<div class="container">-->
 
-      <!-- Main hero unit for a primary marketing message or call to action -->
-      <div class="hero-unit">
-        <h1>Hello, world!</h1>
-        <p>This is a template for a simple marketing or informational website. It includes a large callout called the hero unit and three supporting pieces of content. Use it as a starting point to create something more unique.</p>
-        <p><a href="#" class="btn btn-primary btn-large">Learn more &raquo;</a></p>
-      </div>
+      <!--[> Main hero unit for a primary marketing message or call to action <]-->
+      <!--<div class="hero-unit">-->
+        <!--<h1>Hello, world!</h1>-->
+        <!--<p>This is a template for a simple marketing or informational website. It includes a large callout called the hero unit and three supporting pieces of content. Use it as a starting point to create something more unique.</p>-->
+        <!--<p><a href="#" class="btn btn-primary btn-large">Learn more &raquo;</a></p>-->
+      <!--</div>-->
 
-      <!-- Example row of columns -->
-      <div class="row">
-        <div class="span4">
-          <h2>Heading</h2>
-          <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-          <p><a class="btn" href="#">View details &raquo;</a></p>
-        </div>
-        <div class="span4">
-          <h2>Heading</h2>
-          <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-          <p><a class="btn" href="#">View details &raquo;</a></p>
-       </div>
-        <div class="span4">
-          <h2>Heading</h2>
-          <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
-          <p><a class="btn" href="#">View details &raquo;</a></p>
-        </div>
-      </div>
+      <!--[> Example row of columns <]-->
+      <!--<div class="row">-->
+        <!--<div class="span4">-->
+          <!--<h2>Heading</h2>-->
+          <!--<p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>-->
+          <!--<p><a class="btn" href="#">View details &raquo;</a></p>-->
+        <!--</div>-->
+        <!--<div class="span4">-->
+          <!--<h2>Heading</h2>-->
+          <!--<p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>-->
+          <!--<p><a class="btn" href="#">View details &raquo;</a></p>-->
+       <!--</div>-->
+        <!--<div class="span4">-->
+          <!--<h2>Heading</h2>-->
+          <!--<p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>-->
+          <!--<p><a class="btn" href="#">View details &raquo;</a></p>-->
+        <!--</div>-->
+      <!--</div>-->
 
       <hr>
 
@@ -106,22 +106,8 @@
 
     </div> <!-- /container -->
 
-    <!-- Le javascript
-    ================================================== -->
-    <!-- Placed at the end of the document so the pages load faster -->
-    <script src='<?=base_url().'bootstrap/assets/js/jquery.js'?>'></script>
-    <script src='<?=base_url().'bootstrap/assets/js/bootstrap-transition.js'?>'></script>
-    <script src='<?=base_url().'bootstrap/assets/js/bootstrap-alert.js'?>'></script>
-    <script src='<?=base_url().'bootstrap/assets/js/bootstrap-modal.js'?>'></script>
-    <script src='<?=base_url().'bootstrap/assets/js/bootstrap-dropdown.js'?>'></script>
-    <script src='<?=base_url().'bootstrap/assets/js/bootstrap-scrollspy.js'?>'></script>
-    <script src='<?=base_url().'bootstrap/assets/js/bootstrap-tab.js'?>'></script>
-    <script src='<?=base_url().'bootstrap/assets/js/bootstrap-tooltip.js'?>'></script>
-    <script src='<?=base_url().'bootstrap/assets/js/bootstrap-popover.js'?>'></script>
-    <script src='<?=base_url().'bootstrap/assets/js/bootstrap-button.js'?>'></script>
-    <script src='<?=base_url().'bootstrap/assets/js/bootstrap-collapse.js'?>'></script>
-    <script src='<?=base_url().'bootstrap/assets/js/bootstrap-carousel.js'?>'></script>
-    <script src='<?=base_url().'bootstrap/assets/js/bootstrap-typeahead.js'?>'></script>
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+    <script src='<?=base_url().'bootstrap/js/bootstrap.js'?>'></script>
 
   </body>
 </html>
