5136dd614faabf806a8a11197e4e5a8bcaf82365
mht <qrnnis2623891@gmail.com>
2013-03-26 22:05:02 +0800

    update

--- application/views/authorization_view.php
+++ application/views/authorization_view.php
@@ -1,79 +1,77 @@
 <!DOCTYPE html>
 <html>
-  <head>
-    <meta charset="utf-8">
-    <title>Bootstrap, from Twitter</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="">
-    <meta name="author" content="">
+    <head>
+        <meta charset="utf-8">
+        <title>Bootstrap, from Twitter</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="description" content="">
+        <meta name="author" content="">
 
-    <!-- Le styles -->
-    <link href='<?=base_url().'bootstrap/assets/css/bootstrap.css'?>' type="text/css" rel = "stylesheet"></link>
-    <style type="text/css">
-      body {
-        padding-top: 60px;
-        padding-bottom: 40px;
-      }
-    </style>
-    <link href='<?=base_url().'bootstrap/assets/css/bootstrap-reponsive.css'?>' type="text/css" rel = "stylesheet"></link>
+        <!-- Le styles -->
+        <link href='<?=base_url().'bootstrap/assets/css/bootstrap.css'?>' type="text/css" rel = "stylesheet"></link>
+        <style type="text/css">
+            body {
+                padding-top: 60px;
+                padding-bottom: 40px;
+            }
+        </style>
+        <link href='<?=base_url().'bootstrap/assets/css/bootstrap-reponsive.css'?>' type="text/css" rel = "stylesheet"></link>
 
-    <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
-    <!--[if lt IE 9]>
-      <script src="assets/js/html5shiv.js"></script>
-    <![endif]-->
+        <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
+        <!--[if lt IE 9]>
+        <script src="assets/js/html5shiv.js"></script>
+        <![endif]-->
 
-    <!-- Fav and touch icons -->
-    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="assets/ico/apple-touch-icon-144-precomposed.png">
-    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="assets/ico/apple-touch-icon-114-precomposed.png">
-      <link rel="apple-touch-icon-precomposed" sizes="72x72" href="assets/ico/apple-touch-icon-72-precomposed.png">
-                    <link rel="apple-touch-icon-precomposed" href="assets/ico/apple-touch-icon-57-precomposed.png">
-                                   <link rel="shortcut icon" href="assets/ico/favicon.png">
-  </head>
+        <!-- Fav and touch icons -->
+        <link rel="apple-touch-icon-precomposed" sizes="144x144" href="assets/ico/apple-touch-icon-144-precomposed.png">
+        <link rel="apple-touch-icon-precomposed" sizes="114x114" href="assets/ico/apple-touch-icon-114-precomposed.png">
+        <link rel="apple-touch-icon-precomposed" sizes="72x72" href="assets/ico/apple-touch-icon-72-precomposed.png">
+        <link rel="apple-touch-icon-precomposed" href="assets/ico/apple-touch-icon-57-precomposed.png">
+        <link rel="shortcut icon" href="assets/ico/favicon.png">
+    </head>
 
-  <body>
+    <body>
 
-    <div class="navbar navbar-inverse navbar-fixed-top">
-      <div class="navbar-inner">
-        <div class="container">
-          <button type="button" class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-          <a class="brand" href="#">資料庫系統實驗室</a>
-          <div class="nav-collapse collapse">
-            <ul class="nav">
-              <li class="active"><a href="#">首頁</a></li>
-              <li><a href="#about">關於</a></li>
-              <li><a href="#contact">聯絡我們</a></li>
-              <li class="dropdown">
-                <a href="#" class="dropdown-toggle" data-toggle="dropdown">服務<b class="caret"></b></a>
-                <ul class="dropdown-menu">
-                  <li><a href="#">管理書籍</a></li>
-                  <li><a href="home/searchBooks">查詢書籍</a></li>
-                  <li><a href="#">還書</a></li>
-                  <li><a href="<?echo site_url('/home/listbooks')?>">個人書櫃</a></li>
-    <!--<a href="<?= site_url('/main/create'); ?>"> Create </a>-->
-                <!--<li><?= anchor('/home/listbooks', 'Create'); ?></li>-->
-                  <li><a href="#">預約紀錄</a></li>
-                  <li class="divider"></li>
-                  <li class="nav-header">有關帳戶</li>
-                  <li><a href="#">移除帳戶</a></li>
-                  <li><a href="#">修改密碼</a></li>
-                </ul>
-              </li>
-            <li><a href="home/logout">Logout</a></li> 
-            </ul>
-          </div><!--/.nav-collapse -->
+        <div class="navbar navbar-inverse navbar-fixed-top">
+            <div class="navbar-inner">
+                <div class="container">
+                    <button type="button" class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="brand" href="#">資料庫系統實驗室</a>
+                    <div class="nav-collapse collapse">
+                        <ul class="nav">
+                            <li class="active"><a href="#">首頁</a></li>
+                            <li><a href="#about">關於</a></li>
+                            <li><a href="#contact">聯絡我們</a></li>
+                            <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown">服務<b class="caret"></b></a>
+                            <ul class="dropdown-menu">
+                                <li><a href="#">管理書籍</a></li>
+                                <li><a href="home/searchBooks">查詢書籍</a></li>
+                                <li><a href="#">還書</a></li>
+                                <li><?= anchor('/home/listbooks', '個人書櫃'); ?></li>
+                                <li><a href="#">預約紀錄</a></li>
+                                <li class="divider"></li>
+                                <li class="nav-header">有關帳戶</li>
+                                <li><a href="#">移除帳戶</a></li>
+                                <li><a href="#">修改密碼</a></li>
+                            </ul>
+                            </li>
+                            <li><?= anchor('/home/Logout', 'Logout'); ?></li>
+                        </ul>
+                    </div><!--/.nav-collapse -->
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
 
-      <hr>
+        <hr>
 
-      <footer>
+        <footer>
         <p>&copy; Company 2013</p>
-      </footer>
+        </footer>
 
     </div> <!-- /container -->
 
@@ -83,5 +81,5 @@
     <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
     <script src='<?=base_url().'bootstrap/js/bootstrap.js'?>'></script>
 
-  </body>
+</body>
 </html>
