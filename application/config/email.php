f8061c704f7766f221106c697b2d17a102347458
mht <qrnnis2623891@gmail.com>
2013-04-04 11:27:22 +0800

    update

--- application/config/email.php
+++
@@ -1,9 +0,0 @@
-<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-$config['protocol'] = 'smtp';
-$config['smtp_host'] = 'ssl://smtp.gmail.com';
-$config['smtp_user'] = 'qrnnis2623891@gmail.com';
-$config['smtp_pass'] = 'gm2547881';
-$config['smtp_port'] = '465';
-$config['smtp_timeout'] = '5';
-$config['newline'] = "\r\n";
-$config['crlf'] = "\r\n";
