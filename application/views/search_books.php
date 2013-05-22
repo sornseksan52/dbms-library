0297e9d6b8325fe6b3cd38101f80926ebdf2ef32
mht <qrnnis2623891@gmail.com>
2013-05-22 15:27:31 +0800

    update

--- application/views/search_books.php
+++ application/views/search_books.php
@@ -96,7 +96,7 @@
         if(is_array($query)){
         foreach($query as $row){
         if(!in_array($row->number,$borrowed)){
-            echo '<tr>';
+            echo '<tr class = "warning">';
             echo '<td><input type="checkbox" name=borrow_books[] value ='. "'$row->number'" .'/></td>';
         }else{
             echo '<tr class = "success">';
